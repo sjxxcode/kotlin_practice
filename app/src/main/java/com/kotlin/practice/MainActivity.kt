416dbac03fcacf01de6e846f42416f2be00769a4
sujian03 <sujian03@zuoyebang.com>
2023-04-06 22:08:32 +0800

    add: 合并commit1

--- app/src/main/java/com/kotlin/practice/MainActivity.kt
+++ app/src/main/java/com/kotlin/practice/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.kotlin.practice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kotlin.practice.databinding.ActivityC1Binding
+import com.kotlin.practice.databinding.ActivityMainBinding
+import com.kotlin.practice.rengwuxian.C1Activity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnTest1.setOnClickListener {
+            startActivity(Intent(this@MainActivity, C1Activity::class.java))
+        }
     }
 }
